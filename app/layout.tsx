0a1ef36c7efca15bf9e4d94a51c92dc75e3c5631
via tekh <56348559+viatekh@users.noreply.github.com>
2026-06-03 17:05:33 +0100

    fixed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,15 +13,23 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <AppBar position="static" sx={{ mb: 3, bgcolor: '#6443cb' }}>
           <Toolbar>
-            <Typography variant="h6" sx={{ flexGrow: 1 }}>
-              praeLink
-            </Typography>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>praeLink</Typography>
             <Stack direction="row" spacing={2}>
-              <Button color="inherit" component={Link} href="/">Dashboard</Button>
-              <Button color="inherit" component={Link} href="/inventory">Inventory</Button>
-              <Button color="inherit" component={Link} href="/packages">Packages</Button>
-              <Button color="inherit" component={Link} href="/clients">Clients</Button>
-              <Button color="inherit" component={Link} href="/projects">Projects</Button>
+              <Link href="/" passHref legacyBehavior>
+                <Button color="inherit">Dashboard</Button>
+              </Link>
+              <Link href="/inventory" passHref legacyBehavior>
+                <Button color="inherit">Inventory</Button>
+              </Link>
+              <Link href="/packages" passHref legacyBehavior>
+                <Button color="inherit">Packages</Button>
+              </Link>
+              <Link href="/clients" passHref legacyBehavior>
+                <Button color="inherit">Clients</Button>
+              </Link>
+              <Link href="/projects" passHref legacyBehavior>
+                <Button color="inherit">Projects</Button>
+              </Link>
             </Stack>
           </Toolbar>
         </AppBar>
